aa4f84ed912e18250942a7259df23e5530513174
populary <410445919@qq.com>
2016-08-01 23:46:40 +0800

    Initial Commit

---
+++ YX_NestViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YX_NestViewController
+//
+//  Created by yang on 16/8/1.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YX_NestViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YX_NestViewController
+//
+//  Created by yang on 16/8/1.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
